5999c808c330f4f14479da6ca45cb149a1bf4977
Muhammad Azizol Aminuddin <bjoe88@gmail.com>
2021-01-10 13:40:00 +0100

    Remove debug command
    Remove debug command

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,9 +16,6 @@ then
   touch "$SSHPATH/known_hosts"
 fi
 
-value=$(</github/workspace/docker-compose.prod)
-echo "${value//:DOCKER_TAG/$INPUT_SHA8}" > $HOME/docker_compose_${INPUT_SHA8}.yml
-
 echo "$INPUT_KEY" > "$SSHPATH/deploy_key"
 chmod 700 "$SSHPATH"
 chmod 600 "$SSHPATH/known_hosts"
@@ -29,7 +26,6 @@ echo Start Run Command
 
 if [ "$INPUT_PASS" = "" ]
 then
-  sh -c "scp -i $SSHPATH/deploy_key -o StrictHostKeyChecking=no -p $INPUT_PORT $HOME/docker_compose_${INPUT_SHA8}.yml ${INPUT_USER}@${INPUT_HOST}:/home/gha"
   sh -c "ssh -i $SSHPATH/deploy_key -o StrictHostKeyChecking=no -p $INPUT_PORT ${INPUT_USER}@${INPUT_HOST} < $HOME/shell.sh"
 else
   sh -c "sshpass -p $INPUT_PASS ssh -o StrictHostKeyChecking=no -p $INPUT_PORT ${INPUT_USER}@${INPUT_HOST} < $HOME/shell.sh"
